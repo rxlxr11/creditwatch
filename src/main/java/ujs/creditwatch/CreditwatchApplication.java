8ea9a4c7881471916ec92d74cfe7c2ed483bd712
rxlxr <77777777@qq.com>
2025-03-08 15:02:09 +0800

    3-8 15:02

---
+++ src/main/java/ujs/creditwatch/CreditwatchApplication.java
@@ -0,0 +1,13 @@
+package ujs.creditwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditwatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditwatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ujs/creditwatch/CreditwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ujs.creditwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditwatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
